8fcdd20cf76d35586573e2e3084a5a69b7317efb
Humberto Meireles <m3ireles@gmail.com>
2022-07-03 18:46:17 -0300

    remove comments

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ print(" ---- Yes: press [1]   |   No: press [0] ----- ")
 print(" --------------------------------------------- ")
 print()
 
-# (A) Did you call the victim?
+
 
 call = int(input("=> Did you call the victim? Yes [1] , No [0]:  "))
 print()
 
-# (B) Have you been in the crime scene?
+
 
 scene = int(input("=> Have you been in the crime scene? Yes [1] , No [0]:  "))
 print()
 
-# (C) Do you live near the victim's home?
+
 
 live = int(input("=> Do you live near the victim's home? Yes [1] , No [0]:  "))
 print()
 
-# (D) Did you owe money or other thing to the victim?
+
 
 owe = int(input("=> Did you owe money or other thing to the victim? Yes [1] , No [0]:  "))
 print()
 
-# (E) Have you worked with the victim?
+
 
 work = int(input("=> Have you worked with the victim? Yes [1] , No [0]:  "))
 print()
@@ -36,13 +36,13 @@ print()
 print(" --------------------------------------------- ")
 
 
-### CLASSIFICATION ###
+
 
 classification = call + scene + live + owe + work
 
 print("=> Classification: ", classification)
 
-# INOCENT, SUSPECT, ACCOMPLICE or MURDERER:
+
 
 if(classification < 2):
 	print("=> The person is INOCENT.")
@@ -56,4 +56,3 @@ else:
 
 
 print(" --------------------------------------------- ")
-
